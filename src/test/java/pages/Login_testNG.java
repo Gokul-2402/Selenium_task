e961dd10525d0b99861a151206c37e34e999915d
“[firstname <[valid-email]>
2025-08-29 11:59:23 +0530

    changes

--- src/test/java/pages/Login_testNG.java
+++ src/test/java/pages/Login_testNG.java
@@ -15,76 +15,71 @@ import org.testng.annotations.AfterTest;
 
 public class Login_testNG {
 	WebDriver Driver;
-	
-	
-	By userName=  By.id("username");
+
+	By userName = By.id("username");
 	By mailID = By.id("email");
 	By phn = By.id("tel");
 	By file = By.name("datafile");
-	By experience =By.xpath("//input[@value='three']");
-	
+	By experience = By.xpath("//input[@value='three']");
+
 	By click = By.id("submit");
-	
-	public Login_testNG(WebDriver Driver ) {
-		  this.Driver = Driver;
-		  }
-	
-  @Test
 
-public String user(String input) {
-	  Driver.findElement(userName).sendKeys(input);
-	  return  Driver.findElement(userName).getAttribute("value");
+	public Login_testNG(WebDriver Driver) {
+		this.Driver = Driver;
 	}
-  public String mail(String input) {
-	  Driver.findElement(mailID).sendKeys(input);
-	  return Driver.findElement(mailID).getAttribute("value");
-	  
-	}
-  public String phone(String input) {
-	  Driver.findElement(phn).sendKeys(input);
-	  return Driver.findElement(phn).getAttribute("value");
-	  
-	}
-  public  String Files(String input ) {
-	  Driver.findElement(file).sendKeys(input);
-	  return Driver.findElement(file).getAttribute("value");
-	 }
 
-  public String gender(String input) {
-     Select gender = new Select(Driver.findElement(By.name("sgender")));
-        gender.selectByContainsVisibleText(input);
-        return Driver.findElement(By.name("sgender")).getAttribute("value");
-        
-       
-     }
- 
-  public boolean Exp() {
-	  Driver.findElement(By.xpath("//input[@value='three']")).click();
-	 return Driver.findElement(By.xpath("//input[@value='three']")).isSelected();
+	@Test
+
+	public String user(String input) {
+		Driver.findElement(userName).sendKeys(input);
+		return Driver.findElement(userName).getAttribute("value");
 	}
-  public boolean Skills() {
-	  Driver.findElement(By.xpath("//input[@value='testng']")).click();
-	  return Driver.findElement(By.xpath("//input[@value='testng']")).isSelected();
-	  
+
+	public String mail(String input) {
+		Driver.findElement(mailID).sendKeys(input);
+		return Driver.findElement(mailID).getAttribute("value");
+
 	}
-  public String automation(String input) {
-	     Select automatic= new Select(Driver.findElement(By.id("tools")));
-	     automatic.selectByContainsVisibleText(input);
-	     return Driver.findElement(By.id("tools")).getAttribute("value");
-	     
-		  
-		}
-  public void click() {
-	  Driver.findElement(By.id("submit")).click();
-	
-	  
+
+	public String phone(String input) {
+		Driver.findElement(phn).sendKeys(input);
+		return Driver.findElement(phn).getAttribute("value");
+
 	}
-  
-  
 
-	  
-  }
+	public String Files(String input) {
+		Driver.findElement(file).sendKeys(input);
+		return Driver.findElement(file).getAttribute("value");
+	}
 
- 
+	public String gender(String input) {
+		Select gender = new Select(Driver.findElement(By.name("sgender")));
+		gender.selectByContainsVisibleText(input);
+		return Driver.findElement(By.name("sgender")).getAttribute("value");
 
+	}
 
+	public boolean Exp() {
+		Driver.findElement(By.xpath("//input[@value='three']")).click();
+		return Driver.findElement(By.xpath("//input[@value='three']")).isSelected();
+	}
+
+	public boolean Skills() {
+		Driver.findElement(By.xpath("//input[@value='testng']")).click();
+		return Driver.findElement(By.xpath("//input[@value='testng']")).isSelected();
+
+	}
+
+	public String automation(String input) {
+		Select automatic = new Select(Driver.findElement(By.id("tools")));
+		automatic.selectByContainsVisibleText(input);
+		return Driver.findElement(By.id("tools")).getAttribute("value");
+
+	}
+
+	public void click() {
+		Driver.findElement(By.id("submit")).click();
+
+	}
+
+}
--- src/test/java/test/Run_LoginTestNG.java
+++ src/test/java/test/Run_LoginTestNG.java
@@ -20,78 +20,60 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Run_LoginTestNG {
-	
-	
-	 Properties p = new Properties();
-	 
-	 WebDriver Driver = new ChromeDriver();
-	
-	 
-	 
-	
-	 
-	 
-  @Test
-  public void f() throws IOException, InterruptedException {
-	  
-	 
-	  
-	  Login_testNG newobj =  new Login_testNG(Driver);
-	   String wuname =newobj.user(p.getProperty("username"));
-	   System.out.println(wuname);
-	  String wmail = newobj.mail(p.getProperty("mail"));
-	  System.out.println(wmail);
-	  String wphn = newobj.phone(p.getProperty("number"));
-	  System.out.println(wphn);
-	   String Wfile =newobj.Files(p.getProperty("upload"));
-	   System.out.println(Wfile);
-	  String Wgender =newobj.gender(p.getProperty("gender"));
-	  System.out.println(Wgender);
-	   Boolean WExp = newobj.Exp();
-	   System.out.println(WExp);
-	  Boolean Wskills = newobj.Skills();
-	  System.out.println(Wskills);
-	   String Wauto = newobj.automation(p.getProperty("Auto"));
-	   System.out.println(Wauto);
-	   Thread.sleep(3000);
-	  newobj.click();
-	
-	  
-	 
-  }
-  @BeforeTest
-  public void beforetest() throws IOException, InterruptedException {
-	 
-	  FileReader fr = new FileReader("C:\\Users\\gokulakrishnan.v\\Documents\\Changepond\\Maven_projectTask\\MavenTask\\utility\\login.properties");
-		p.load(fr);
-		
-		
-		  Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		  String first = Driver.getWindowHandle();
-		  Driver.get(p.getProperty("link"));
-		  Driver.manage().window().maximize();
-		  
-		  Driver.switchTo().newWindow(WindowType.TAB);
-		  String second = Driver.getWindowHandle();
-		  Driver.get(p.getProperty("link1"));
-		  Driver.switchTo().window(first);
-		  
-		  
-		  
-		  
-		 
-		  
-		  Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		  	
-  	 
-  	  
-  }
+	Properties p = new Properties();
+	WebDriver Driver = new ChromeDriver();
 
-  @AfterTest
-  public void afterTest() {
-	 
-	  
-	  
-  	  
-  }
+	@Test
+	public void login() throws IOException, InterruptedException {
+		Login_testNG newobj = new Login_testNG(Driver);
+		String wuname = newobj.user(p.getProperty("username"));
+		System.out.println(wuname);
+
+		String wmail = newobj.mail(p.getProperty("mail"));
+		System.out.println(wmail);
+
+		String wphn = newobj.phone(p.getProperty("number"));
+		System.out.println(wphn);
+
+		String Wfile = newobj.Files(p.getProperty("upload"));
+		System.out.println(Wfile);
+
+		String Wgender = newobj.gender(p.getProperty("gender"));
+		System.out.println(Wgender);
+		Boolean WExp = newobj.Exp();
+		System.out.println(WExp);
+		Boolean Wskills = newobj.Skills();
+		System.out.println(Wskills);
+		String Wauto = newobj.automation(p.getProperty("Auto"));
+		System.out.println(Wauto);
+		Thread.sleep(3000);
+		newobj.click();
+
+	}
+
+	@BeforeTest
+	public void beforetest() throws IOException, InterruptedException {
+
+		FileReader fr = new FileReader(
+				"C:\\Users\\gokulakrishnan.v\\Documents\\Changepond\\Maven_projectTask\\MavenTask\\utility\\login.properties");
+		p.load(fr);
+
+		Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String first = Driver.getWindowHandle();
+		Driver.get(p.getProperty("link"));
+		Driver.manage().window().maximize();
+
+		Driver.switchTo().newWindow(WindowType.TAB);
+		String second = Driver.getWindowHandle();
+		Driver.get(p.getProperty("link1"));
+		Driver.switchTo().window(first);
+
+		Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+	}
 }
